fddcb0306acbb529cf8e837f5c760ade1d45acd9
49021_Pratik <rajpurepratik1@gmail.com>
2026-08-29 17:44:02 +0530

    Implement Version 1 voting app

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+votes = {}
+
+
+@app.route("/")
+def home():
+    return "Welcome to the App"
+
+
+@app.route("/health")
+def health():
+    return "App is running"
+
+
+@app.route("/vote/<name>")
+def vote(name):
+    if name in votes:
+        votes[name] += 1
+    else:
+        votes[name] = 1
+
+    return jsonify({
+        "message": f"Vote recorded for {name}",
+        "votes": votes[name]
+    })
+
+
+@app.route("/results")
+def results():
+    return jsonify(votes)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
